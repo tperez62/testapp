c3046525b34565a0070dc09bc29671a28e348be3
tperez62 <perezt1994@gmail.com>
2015-10-14 16:08:02 -0400

    Added backup_to_csv method

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,18 @@
+def backup_to_csv(name)
+	csvname = "#{name}.csv"
+	CSV.open(csvname,'w') do |csv|
+		headers = ['Created At','Request Url','Response Code']
+		csv << headers
+		AppEvent.all.each do |event|
+			a = []
+			a << event.created_at
+			a << event.request_url
+			a << event.response_code
+			csv << a
+		end
+	end
+end
+
 namespace :db do
 	task :backup => [:backupapp, :backupapp2] do
 		#Back Up App & App2 AppEvent tables
@@ -5,31 +20,11 @@ namespace :db do
 
 	task :backupapp do
 		require './app'
-		CSV.open('app_db.csv','w') do |csv|
-			headers = ['Created At','Request Url','Response Code']
-			csv << headers
-			AppEvent.all.each do |event|
-				a = []
-				a << event.created_at
-				a << event.request_url
-				a << event.response_code
-				csv << a
-			end
-		end
+		backup_to_csv('app_db')
 	end
 	
 	task :backupapp2 do
 		require './app2'
-		CSV.open('app2_db.csv','w') do |csv|
-			headers = ['Created At','Request Url','Response Code']
-			csv << headers
-			AppEvent.all.each do |event|
-				a = []
-				a << event.created_at
-				a << event.request_url
-				a << event.response_code
-				csv << a
-			end
-		end
+		backup_to_csv('app2_db')
 	end
 end
